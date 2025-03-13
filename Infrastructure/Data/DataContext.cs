3731882e85a4690b1b9dac94816ee657380e99cf
NachoXx25 <ignacio.alfonso2002@gmail.com>
2025-03-13 18:05:18 -0300

    feat: add initial database configuration

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Proyecto_web_api.Domain.Models;
+
+namespace Proyecto_web_api.Infrastructure.Data
+{
+    public class DataContext : IdentityDbContext<User,Role, int>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostFile> PostFiles { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Reaction> Reactions { get; set; }
+        public DbSet<ReactionType> ReactionTypes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,61 @@
+using dotenv.net;
+using dotenv.net.Utilities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_web_api.Domain.Models;
+using Proyecto_web_api.Infrastructure.Data;
+
+DotEnv.Load();
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseNpgsql(EnvReader.GetStringValue("PostgreSQLConnection")));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentity<User, Role>().AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireNonAlphanumeric = false;
+        options.User.RequireUniqueEmail = true;
+    }
+);
 
 var app = builder.Build();
+
+//Configuración para los seeders
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<DataContext>();
+    dbContext.Database.Migrate();
+    await DataSeeder.Initialize(services);
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 
-
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthorization();
+app.UseAuthentication();
+app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
